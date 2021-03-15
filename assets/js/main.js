49070c6b0d7eeca9397d6614e037e56c5225c5ba
Kari Thrastarson <Kari.Thrastarson@islandsbanki.is>
2021-03-15 16:14:03 +0100

    Draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-	var x = document.getElementById("topnav");
-	if (x.className === "topnav") {
-	  x.className += " responsive";
-	} else {
-	  x.className = "topnav";
-	}
-  }
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
-  function toggleBurger() {
-	var element = document.getElementById("hamburger");
-	element.classList.toggle("is-active");
+function toggleBurger() {
+    var element = document.getElementById("hamburger");
+    element.classList.toggle("is-active");
 
-	var x = document.getElementById("topnav");
-	if (x.className === "topnav") {
-	  x.className += " responsive";
-	} else {
-	  x.className = "topnav";
-	}
-  }
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
