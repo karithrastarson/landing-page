24702e0760888aadd15d7ba1037b2719913688e6
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2021-03-18 15:07:26 +0100

    NEW MENU

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,22 @@ function toggleBurger() {
     var element = document.getElementById("hamburger");
     element.classList.toggle("is-active");
 
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+    openNav();
+
+    // var x = document.getElementById("topnav");
+    // if (x.className === "topnav") {
+    //     x.className += " responsive";
+    // } else {
+    //     x.className = "topnav";
+    // }
+}
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("hamburger").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("hamburger").style.display = "block";
 }
