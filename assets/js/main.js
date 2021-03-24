abed78176f2d75fc45c7a682037e03a1b938bc66
karithrastarson <karithrastarson@gmail.com>
2021-03-24 22:29:09 +0100

    registry draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,54 @@ function flip() {
 function flipBack() {
     document.getElementById("flip-box-inner").style.transform = "rotateY(360deg)";
 }
+
+async function getAllItems() {
+    const response = await fetch('http://localhost:8484/register');
+    const myJson = await response.json(); //extract JSON from the http
+    console.log("Successs:");
+
+    for (var i = 0; i < myJson.length; i++) {
+        var obj = myJson[i];
+
+        console.log(obj.name);
+    }
+
+}
+
+async function buildHtmlTable(selector) {
+    const response = await fetch('http://localhost:8484/register');
+    const myList = await response.json();
+
+    var columns = addAllColumnHeaders(myList, selector);
+
+    for (var i = 0; i < myList.length; i++) {
+        var row$ = $('<tr/>');
+        for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+            var cellValue = myList[i][columns[colIndex]];
+            if (cellValue == null) cellValue = "";
+            row$.append($('<td/>').html(cellValue));
+        }
+        $(selector).append(row$);
+    }
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+function addAllColumnHeaders(myList, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+
+    for (var i = 0; i < myList.length; i++) {
+        var rowHash = myList[i];
+        for (var key in rowHash) {
+            if ($.inArray(key, columnSet) == -1) {
+                columnSet.push(key);
+                headerTr$.append($('<th/>').html(key));
+            }
+        }
+    }
+    $(selector).append(headerTr$);
+
+    return columnSet;
+}
